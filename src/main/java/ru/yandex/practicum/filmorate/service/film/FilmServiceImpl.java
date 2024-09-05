22d88d6f38419b3e14288cf4e9bed613836bbcbd
Adamets Aleksei <verwylf@yandex.ru>
2024-09-05 14:32:42 +0300

    Сделать ребейз ветки add-dorectpr

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exceptions.InternalServerException;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.repository.Film.FilmRepository;
 import ru.yandex.practicum.filmorate.repository.Genre.GenreRepository;
