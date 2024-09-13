91bcc080af3d704ddda376b31d4978ddf76243d6
Adamets Aleksei <verwylf@yandex.ru>
2024-09-13 16:07:28 +0300

    Сделать небольшие правки Добавить в схему Составной ключ для таблицы POPUlAR

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import ru.yandex.practicum.filmorate.exceptions.InternalServerException;
